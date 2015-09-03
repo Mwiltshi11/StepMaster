0f428ba4bf6fbaa251a1f593b1f67e9a6cd15618
Mark Wiltshire <Mwiltshi11@gmail.com>
2015-09-03 11:16:02 -0700

    notes

---
+++ src/classAverage/JavaClassAverage.java
@@ -0,0 +1,26 @@
+package classAverage;
+
+public class JavaClassAverage {
+
+	private static int test1=75;
+	private static int test2=85;
+	private static float sum=0;
+	private static float average=0;
+	
+	public static void main (String[] args) 
+			
+			
+	{
+	
+		average.sum();
+	}
+		
+	public static float sum ()
+	{
+		
+		average=(test1+test2)/2;
+		return(average);
+		
+	
+}
+}
