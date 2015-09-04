8d1b9efdff09b56ed013c3046c5c6c6e9b031b4b
Mark Wiltshire <Mwiltshi11@gmail.com>
2015-09-04 10:07:17 -0700

    Order Checker revised

--- src/stepdummy/OrderChecker.java
+++ src/stepdummy/OrderChecker.java
@@ -2,26 +2,38 @@ package stepdummy;
 
 import java.util.Scanner;
 
-public class OrderChecker {
-	public static void main (String[] args ) {
-	
-	
-	double numberOfBolts = 0,numberOfNuts = 0,numberOfWashers = 0;
-	double priceOfBolts =.05,priceOfNuts =.03,priceOfWashers=.01;
-	double sum = 0;
-	Scanner scan = new Scanner(System.in);
-	
-	
-		System.out.println("How many bolts,nuts, and washers in purchase?");
-	
-	if(numberOfNuts<=numberOfBolts) {
-		System.out.println("Check the order: too few nuts");
-	}
-	
-	if(numberOfWashers < (numberOfBolts * 2)) {
-		System.out.println("Check the order; too few washers");
+public class OrderChecker 
+{
+	public static void main (String[] args ) 
+	{
 		
-											  }
-											}
-	
-							}
+		double numberOfBolts = 0,numberOfNuts = 0,numberOfWashers = 0;
+		double priceOfBolts =.05,priceOfNuts =.03,priceOfWashers=.01;
+		double sum; 
+		Scanner input= new Scanner(System.in);
+		
+		for( int i=0; i<3; i++)
+		{
+			
+			System.out.println("How many bolts in purchase?");
+			numberOfBolts = input.nextDouble();
+			System.out.println("How many nuts in purchase?");
+			numberOfNuts = input.nextDouble();
+			System.out.println("How many washers in purchase?");
+			numberOfWashers = input.nextDouble();
+			
+			if(numberOfNuts < numberOfBolts) 
+				System.out.println("Check the order: too few nuts");
+			if(numberOfWashers < (numberOfBolts * 2) )
+				System.out.println("Check the order; too few washers");
+			if( !(numberOfNuts < numberOfBolts) && !(numberOfWashers < (numberOfBolts * 2) ) )
+				System.out.println("Order is ok");
+			
+			sum = (priceOfBolts * numberOfBolts) + (priceOfNuts * numberOfNuts) + (priceOfWashers * numberOfWashers);
+			
+			System.out.println("Total Cost " + sum);
+			
+		}
+		
+	}
+}
